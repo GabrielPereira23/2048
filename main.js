b6073a48ccb125e22cc858aecf7b5341f5478aeb
Gabriel Goulart Pereira <gabrielgoulartpereira23@gmail.com>
2023-02-06 18:36:16 -0300

    Método que soma os blocos

--- main.js
+++ main.js
@@ -18,25 +18,30 @@ const DOM = {
 /* --- Jogo --- */
 const game = {
   matrix: [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [2, 4, 0, 0],
+    [2, 4, 1024, 1024],
+    [2, 2, 0, 0],
+    [0, 0, 2, 2],
   ],
   getEmptyPositions: function () {
     let emptyPositions = [];
-    this.matrix.forEach((line, posLine) => {
-      line.forEach((value, colPos) => {
+    this.matrix.forEach((line, lineIndex) => {
+      line.forEach((value, colIndex) => {
         if (value === 0) {
-          emptyPositions.push([posLine, colPos]);
+          emptyPositions.push([lineIndex, colIndex]);
         }
       });
     });
     return emptyPositions;
   },
-  setValue: function (line, col, value) {
-    this.matrix[line][col] = value;
-    DOM.renderGrid(this.matrix);
+  setValue: function (lineIndex, colIndex, value) {
+    this.matrix[lineIndex][colIndex] = value;
+  },
+  getValue: function (lineIndex, colIndex) {
+    if (lineIndex < 0 || lineIndex > 3 || colIndex < 0 || colIndex > 3) {
+      return 0;
+    }
+    return this.matrix[lineIndex][colIndex];
   },
   clearMatrix: function () {
     this.matrix = [
@@ -47,4 +52,44 @@ const game = {
     ];
     DOM.renderGrid(this.matrix);
   },
+  compress: function (side) {
+    this.matrix.forEach((line, lineIndex) => {
+      line.forEach((value, colIndex) => {
+        if (value === 0) {
+          return;
+        }
+        if (side === "up") {
+          if (value === this.getValue(lineIndex - 1, colIndex)) {
+            this.setValue(lineIndex - 1, colIndex, value * 2);
+            this.setValue(lineIndex, colIndex, 0);
+          }
+          return;
+        }
+        if (side === "down") {
+          if (value === this.getValue(lineIndex + 1, colIndex)) {
+            this.setValue(lineIndex + 1, colIndex, value * 2);
+            this.setValue(lineIndex, colIndex, 0);
+          }
+          return;
+        }
+        if (side === "left") {
+          if (value === this.getValue(lineIndex, colIndex - 1)) {
+            this.setValue(lineIndex, colIndex - 1, value * 2);
+            this.setValue(lineIndex, colIndex, 0);
+          }
+          return;
+        }
+        if (side === "right") {
+          if (value === this.getValue(lineIndex, colIndex + 1)) {
+            this.setValue(lineIndex, colIndex + 1, value * 2);
+            this.setValue(lineIndex, colIndex, 0);
+          }
+          return;
+        }
+      });
+    });
+    DOM.renderGrid(this.matrix);
+  },
 };
+
+DOM.renderGrid(game.matrix);
