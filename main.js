388bfe3e117ab5d46276721c6305ab00dfdbe746
Gabriel Goulart Pereira <gabrielgoulartpereira23@gmail.com>
2023-02-06 17:30:21 -0300

    Metódos para setar os valores e para limpar a matriz

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
 /* --- Manipula o DOM --- */
 const DOM = {
-  grid: document.querySelector('.game'),
-  createBlock: function(value) {
-    let div = document.createElement('div');
-    div.innerText = value === 0 ? '' : value;
+  grid: document.querySelector(".game"),
+  createBlock: function (value) {
+    let div = document.createElement("div");
+    div.innerText = value === 0 ? "" : value;
     div.classList.add(`c${value}`);
     return div;
   },
-  renderGrid: function(matrix) {
-    this.grid.innerHTML = '';
-    matrix.forEach(line => {
-      line.forEach(value => this.grid.appendChild(this.createBlock(value)));
+  renderGrid: function (matrix) {
+    this.grid.innerHTML = "";
+    matrix.forEach((line) => {
+      line.forEach((value) => this.grid.appendChild(this.createBlock(value)));
     });
-  }
-}
+  },
+};
 
 /* --- Jogo --- */
 const game = {
-  matrix: [[2,4,8,16],[32,64,128,256],[512,1024,2048,0],[0,0,0,0]],
-  getEmptyPositions: function() {
+  matrix: [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+  getEmptyPositions: function () {
     let emptyPositions = [];
     this.matrix.forEach((line, posLine) => {
       line.forEach((value, colPos) => {
-        if(value === 0) {
+        if (value === 0) {
           emptyPositions.push([posLine, colPos]);
         }
       });
     });
     return emptyPositions;
-  }
-}
+  },
+  setValue: function (line, col, value) {
+    this.matrix[line][col] = value;
+    DOM.renderGrid(this.matrix);
+  },
+  clearMatrix: function () {
+    this.matrix = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    DOM.renderGrid(this.matrix);
+  },
+};
